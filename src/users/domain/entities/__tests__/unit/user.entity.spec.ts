406348df88d60b35259ae21f156ccb807992b8ee
Italo Costa <italocostajobs@gmail.com>
2024-09-02 09:33:16 -0300

    UserEntity - getters methods unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -6,6 +6,7 @@ describe('UserEntity unit tests', () => {
   let sut: UserEntity
 
   beforeEach(() => {
+    // Arrange
     props = {
       name: faker.person.fullName(),
       email: faker.internet.email(),
@@ -22,5 +23,32 @@ describe('UserEntity unit tests', () => {
       ...props,
       createdAt: expect.any(Date),
     })
+  })
+
+  it('should return the name', () => {
+    // Assert
+    expect(sut.props.name).toBeDefined()
+    expect(sut.props.name).toBe(props.name)
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('should return the email', () => {
+    // Assert
+    expect(sut.props.email).toBeDefined()
+    expect(sut.props.email).toBe(props.email)
+    expect(typeof sut.props.email).toBe('string')
+  })
+
+  it('should return the password', () => {
+    // Assert
+    expect(sut.props.password).toBeDefined()
+    expect(sut.props.password).toBe(props.password)
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('should return the createdAt', () => {
+    // Assert
+    expect(sut.props.createdAt).toBeDefined()
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
   })
 })
