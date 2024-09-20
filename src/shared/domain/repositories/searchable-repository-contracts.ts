6e016697e1e057f7fa5e31968a5d0fc18fb1a13f
Italo Costa <italocostajobs@gmail.com>
2024-09-20 14:10:57 -0300

    Adding search method InMemorySearchableRepository

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -138,5 +138,6 @@ export interface SearchableRepositoryInterface<
   SearchInput = SearchParams,
   SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
+  sortableFields: string[]
   search(props: SearchInput): Promise<SearchOutput>
 }
